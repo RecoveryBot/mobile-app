a452e657e6a7e98343441bf74f4ad9285bebd44f
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-16 13:27:22 -0800

    Setup socket.io

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import SocketIOClient from 'socket.io-client';
+import * as config from './env';
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
+
+  constructor(props) {
+    super(props);
+    this.socket = SocketIOClient(config.server);
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
