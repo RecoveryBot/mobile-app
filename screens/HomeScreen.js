af2a6eda849e20fbe8b905e98baf38b8f91d82c8
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-02-17 02:51:58 -0800

    Get user name from server

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,11 +11,29 @@ import { WebBrowser } from 'expo';
 import styles from '../styles.scss';
 import { TitleBar } from '../components/TitleBar';
 import { HeartCard } from '../components/HeartCard';
+import * as config from "../env";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: <TitleBar></TitleBar>
   };
+
+  state = {
+    name: ''
+  }
+
+  async componentWillMount() {
+    const email = this.props.navigation.getParam('email');
+    const getName = await fetch(`${config.server}user?userId=${email}`, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'omit'
+    });
+    const name = await getName.text();
+    this.setState({
+      name
+    });
+  }
 
   render() {
     const email = this.props.navigation.getParam('email');
